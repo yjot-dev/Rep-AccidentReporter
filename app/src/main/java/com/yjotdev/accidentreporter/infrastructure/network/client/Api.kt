510ab9e39469c022fd48787d5a192bf09454a404
yjot <2010guabo@gmail.com>
2026-03-15 23:56:14 -0500

    Mejoras del código

--- app/src/main/java/com/yjotdev/accidentreporter/infrastructure/network/client/Api.kt
+++ app/src/main/java/com/yjotdev/accidentreporter/infrastructure/network/client/Api.kt
@@ -16,7 +16,7 @@ class Api @Inject constructor(
     @ApplicationContext context: Context
 ) {
     private val url = if (BuildConfig.DEBUG) { "https://192.168.1.20:3000/api/" }
-                      else { "https://accidentreporter-yjotdev.up.railway.app/api/" }
+                      else { "https://servicio-api-ar-557625336820.us-central1.run.app/api/" }
     private val httpsClient = if (BuildConfig.DEBUG) { Client.getUnsafeClient(context) }
                               else { Client.getSafeClient() }
 
